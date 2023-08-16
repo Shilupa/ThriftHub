9bb9689f071f4fc1d7c61c01ae05a6d46ca8e7fd
shilpasy <shilpa.yadav@metropolia.fi>
2023-08-16 20:49:24 +0300

    added logout and registration

---
+++ Utility/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("nischhal.shrestha@metropolia.fi"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            var client = new SendGridClient(SendGridSecret);
+
+            var from = new EmailAddress("nischhal.shrestha@metropolia.fi", "ThriftHub");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            return client.SendEmailAsync(msg);
+
+        }
+    }
+}
+
